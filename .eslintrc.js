b2e2167ec9e9e8fba680c7cb626184da923ba84f
Eunomiac <ryan.o.west@gmail.com>
2018-11-17 14:55:54 -0500

    Day One

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,23 @@
-module.exports = {
-    "extends": "standard"
+export default {
+    "parserOptions": {
+        "ecmaVersion": 6
+    },
+    "extends": "standard",
+    "globals": {
+        "D": true,
+        "Chars": true,
+        "WigglePads": true,
+        "ChatFuncs": true,
+        "Roller": true,
+        "TimeTracker": true,
+        "_": false,
+        "state": true,
+        "setAttrs": false,
+        "getObj": false,
+        "findObjs": false,
+        "filterObjs": false,
+        "getAllObjs": false,
+        "getAttrByName": false,
+        "Campaign": false
+    }
 };
